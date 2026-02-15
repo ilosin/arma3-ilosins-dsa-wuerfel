78b5671f29b90e985cb8d35762fb7dc5371f5378
Ilosin <joscha.kropp@me.com>
2026-02-15 17:55:37 +0100

    mod.cpp auf geforkte Version angepasst

--- Builds/@DiceRollByDive/mod.cpp
+++ Builds/@DiceRollByDive/mod.cpp
@@ -1,5 +1,5 @@
-name		 = "Dive's DnD Dice - RPG Dice Roll for Zeus";								// Name of your mod
-author		 = "Dive";				// Affects Arma 3 Launcher, when the mod are loaded as local
+name		 = "Ilosin's DSA Dice - RPG Dice Roll for Zeus based on The Dark Eye Ruleset";								// Name of your mod
+author		 = "Dive & Ilosin";				// Affects Arma 3 Launcher, when the mod are loaded as local
 
 // Overview text, displayed from the extension menu
 hideName	= 0;	// Hide the extension name
@@ -7,11 +7,11 @@ hidePicture	= 0;	// Hide the extension menu
 
 
 picture = "logo.paa";
-description = "Dive's DnD Dice";
+description = "Ilosin's DSA Dice";
 logo = "logo.paa";
 logoOver = "logo.paa";
-tooltip = "Dive's DnD Dice";
-overview = "Dive's DnD Dice";
+tooltip = "Ilosin's DSA Dice";
+overview = "Ilosin's DSA Dice";
 overviewPicture = "logo.paa";
-overviewText = "Dive's DnD Dice";
+overviewText = "Ilosin's DSA Dice";
 overviewFootnote = "";
